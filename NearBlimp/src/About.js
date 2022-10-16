0eb7ba37f5a3d75a7dd0bb10b6cb5337db7aa882
Bruce Moe <brucemoe1@gmail.com>
2022-10-16 08:01:37 -0500

    Auto deployment

--- NearBlimp/src/About.js
+++ NearBlimp/src/About.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+// import './About.css'
 
 export const About = () => (
-    <div className="about">
-        <div className="row">
-            <div className="member">
-                <div><br /></div>
-                <h2>About us</h2>
-                <div><br /></div>
-            </div>
+  <div className="about">
+    <div className="row">
+      <div className="member">
+        <div>
+          <br />
         </div>
+        <h2>About us</h2>
+        <div>
+          <br />
+        </div>
+      </div>
     </div>
-)
+  </div>
+);
