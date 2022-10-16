9ca913986d10451a78f418f63f70729e00f00cc4
Albin Shrestha <shrestha_albin@yahoo.com>
2022-10-16 08:02:12 -0700

    cleaned up code css

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import "./Home.css";
+import Blimp from "./Images/BlimpTrans.png"
 
 export const Home = () => {
     return (
         <div className="Home">
             <h1>Welcome to <span className="sub">Near</span>Blimp!</h1>
             <h2>Here you will find all of the information you need about Smart Contracts on the <span className="sub">Near</span> Protocol</h2>
+            <div class="flier">
+                <img src={Blimp} alt="Logo" />
+            </div>
         </div >
     );
 };
