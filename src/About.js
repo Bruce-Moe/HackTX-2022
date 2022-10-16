2d3292cdc67d59f668759db6ae4041b4ad85ba5e
Albin Shrestha <shrestha_albin@yahoo.com>
2022-10-16 08:29:08 -0700

    fixed warnings

--- src/About.js
+++ src/About.js
@@ -3,34 +3,10 @@ import React from "react";
 
 export const About = () => (
   <div className="about">
-    <h1>About US</h1>
+    <h1>About <span className="sub">Near</span>Blimp</h1>
 
     <h2>Team Members</h2>
     <h4>Bruce Moe, Albin Shrestha, Andrew Wu, John Mackie</h4>
-
-    <h2>Inspiration</h2>
-    <h4></h4>
-
-    <h2>Bounty</h2>
-    <h4></h4>
-
-    <h2>What it does</h2>
-    <h4></h4>
-
-    <h2>How we built it</h2>
-    <h4></h4>
-
-    <h2>Challenges we ran into</h2>
-    <h4></h4>
-
-    <h2>Accomplishments that we're proud of</h2>
-    <h4></h4>
-
-    <h2>What we learned</h2>
-    <h4></h4>
-
-    <h2>What's next for NearBlimp</h2>
-    <h4></h4>
 
     <h2>GitHub</h2>
     <h4><a href="https://github.com/Bruce-Moe/HackTX-2022">Link</a></h4>
