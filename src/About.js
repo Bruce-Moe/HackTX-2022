84b2e291fd62403024fe216cb63bf635382015bf
Albin Shrestha <shrestha_albin@yahoo.com>
2022-10-16 08:23:31 -0700

    about us

--- src/About.js
+++ src/About.js
@@ -3,16 +3,39 @@ import React from "react";
 
 export const About = () => (
   <div className="about">
-    <div className="row">
-      <div className="member">
-        <div>
-          <br />
-        </div>
-        <h2>About us</h2>
-        <div>
-          <br />
-        </div>
-      </div>
-    </div>
+    <h1>About US</h1>
+
+    <h2>Team Members</h2>
+    <h4>Bruce Moe, Albin Shrestha, Andrew Wu, John Mackie</h4>
+
+    <h2>Inspiration</h2>
+    <h4></h4>
+
+    <h2>Bounty</h2>
+    <h4></h4>
+
+    <h2>What it does</h2>
+    <h4></h4>
+
+    <h2>How we built it</h2>
+    <h4></h4>
+
+    <h2>Challenges we ran into</h2>
+    <h4></h4>
+
+    <h2>Accomplishments that we're proud of</h2>
+    <h4></h4>
+
+    <h2>What we learned</h2>
+    <h4></h4>
+
+    <h2>What's next for NearBlimp</h2>
+    <h4></h4>
+
+    <h2>GitHub</h2>
+    <h4><a href="https://github.com/Bruce-Moe/HackTX-2022">Link</a></h4>
+
+    <h2>DevPost</h2>
+    <h4><a href="https://devpost.com/software/nearblimp">Link</a></h4>
   </div>
 );
